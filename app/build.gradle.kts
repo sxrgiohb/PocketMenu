ae0b3c90de5bb0c64af30e3624d2354961a69004
Sergio Herrero <sergioherrero.trad@gmail.com>
2026-01-26 10:37:47 +0100

    feat: update and add dependencies
    Updated versions for lifecycle and defined new versions for Firebase, Navigation, FirebaseUI, and Glide.
    
    Added the following new dependencies:
    - Jetpack Navigation (fragment and ui)
    - FirebaseUI (database and firestore)
    - Glide (core and compiler)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,28 +46,42 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-    //Variable
-    val lifecycle_version = "2.9.0"
+    // Versions
+    val lifecycle_version = "2.10.0"
+    val firebase_bom_version = "34.6.0"
+    val navigation_version = "2.9.6"
+    val firebase_ui_version = "9.1.1"
+    val glide_version = "5.0.5"
+
 
     //Viewmodel
     implementation("androidx.lifecycle:lifecycle-viewmodel:${lifecycle_version}")
-
     //Livedata
     implementation("androidx.lifecycle:lifecycle-livedata:${lifecycle_version}")
 
     //Firebase BOM
-    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
-
+    implementation(platform("com.google.firebase:firebase-bom:${firebase_bom_version}"))
     //Firebase Authentication
     implementation("com.google.firebase:firebase-auth")
-
     //Cloud Firestore
     implementation("com.google.firebase:firebase-firestore")
-
     //Cloud Storage
     implementation("com.google.firebase:firebase-storage")
-
     //Firebase Analytics
     implementation("com.google.firebase:firebase-analytics")
+
+    //Jetpack Navigation
+    implementation("androidx.navigation:navigation-fragment:${navigation_version}")
+    implementation("androidx.navigation:navigation-ui:${navigation_version}")
+
+    // FirebaseUI for Firebase Realtime Database
+    implementation("com.firebaseui:firebase-ui-database:${firebase_ui_version}")
+    // FirebaseUI for Firestore
+    implementation("com.firebaseui:firebase-ui-firestore:${firebase_ui_version}")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:${glide_version}")
+    annotationProcessor("com.github.bumptech.glide:compiler:${glide_version}")
+
 
 }
