c2f4d23be16a7214b2d0a957fa61ebe8b0f49cb5
Sergio Herrero <sergioherrero.trad@gmail.com>
2026-05-09 21:47:03 +0200

    docs: add comments

--- app/src/main/java/com/example/pocketmenu/data/model/auxiliar/MenuAssignment.java
+++ app/src/main/java/com/example/pocketmenu/data/model/auxiliar/MenuAssignment.java
@@ -4,7 +4,7 @@ import com.example.pocketmenu.data.model.Leftover;
 import com.example.pocketmenu.data.model.Menu;
 import com.example.pocketmenu.data.model.Recipe;
 
-// represents the assignment of a recipe to a specific menu, managing the relationship between the recipe, the menu and any leftovers.
+// Represents the assignment of a recipe to a specific menu, managing the relationship between the recipe, the menu and any leftovers.
 public class MenuAssignment {
     private Menu menu;
     private Recipe recipe;
@@ -15,30 +15,6 @@ public class MenuAssignment {
         this.menu = menu;
         this.recipe = recipe;
         this.leftover = leftover;
-    }
-
-    // Calculates how many portions are left to assign
-    public int getAvailablePortions() {
-        // Total portions - used portions
-        int available = recipe.getPortion() - menu.getUsedPortions();
-        // Checks for existing leftovers
-        if (leftover != null) {
-            available += leftover.getRemainingPortions();
-        }
-        return available;
-    }
-
-    // Consume a portion of the recipe or leftover
-    public void consumePortion() {
-        // Checks for remaining portions
-        if (recipe.getPortion() - menu.getUsedPortions() > 0) {
-            // Consumes one portion
-            menu.setUsedPortions(menu.getUsedPortions() + 1);
-            // Checks for leftovers
-        } else if (leftover != null && leftover.getRemainingPortions() > 0) {
-            // Consumes one portion
-            leftover.setRemainingPortions(leftover.getRemainingPortions() - 1);
-        }
     }
 
     // Getters
